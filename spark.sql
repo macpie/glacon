5d3e10cd434d5368380620ba7f0ab8dfbefe24b5
Macpie <pierre@nova-labs.com>
2025-06-11 16:12:27 -0700

    Working spark

--- spark.sql
+++ spark.sql
@@ -1,13 +1,14 @@
-CREATE TABLE namespace_2025_06_10_14_31_10.orders2 (
+-- See https://iceberg.apache.org/docs/1.9.1/spark-queries/
+CREATE TABLE namespace_2025_06_11_14_46_51.orders2 (
     id INT NOT NULL,
     customer_id INT NOT NULL,
     amount FLOAT NOT NULL,
-    ts TIMESTAMP NOT NULL,
+    ts TIMESTAMP_NTZ NOT NULL,
     order_type INT NOT NULL
 ) PARTITIONED BY (days(ts));
 
 INSERT INTO
-    namespace_2025_06_10_14_31_10.orders1
+    namespace_2025_06_11_14_46_51.orders2
 SELECT
     id,
     customer_id,
@@ -15,25 +16,25 @@ SELECT
     ts,
     order_type
 FROM
-    namespace_2025_06_10_14_31_10.orders;
+    namespace_2025_06_11_14_46_51.orders;
 
 SELECT
     COUNT(*)
 FROM
-    namespace_2025_06_10_14_31_10.orders
+    namespace_2025_06_11_14_46_51.orders
 WHERE
     ts > TIMESTAMP '2025-05-20 00:00:00';
 
 SELECT
     COUNT(*)
 FROM
-    namespace_2025_06_10_14_31_10.orders1
+    namespace_2025_06_11_14_46_51.orders2
 WHERE
     ts > TIMESTAMP '2025-05-20 00:00:00';
 
 SELECT
     COUNT(*)
 FROM
-    namespace_2025_06_10_14_31_10.orders1
+    namespace_2025_06_11_14_46_51.orders2
 WHERE
     ts > TIMESTAMP '2025-05-20';
